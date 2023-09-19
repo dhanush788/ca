9aaf468e27fecafdb2663d422c7fdaf301f1a1bf
AB7zz <abhinavcv007@gmail.com>
2023-09-19 18:31:56 +0530

    responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,10 +20,11 @@ const Hero = () => {
     <div className='spacebg'>
     <div className='px-20 py-20 bg-prple-950'>
         {/* <div className='bg-black bg-opacity-50 absolute z-[20] h-[100%] w-screen left-0 top-0'></div> */}
-        <h1 className='my-5 md:text-[90px] xs:text-3xl text-white font-bold text-white text-center my-5'>DHISHNA CAMPUS AMBASSADOR</h1>
+        <h1 className='my-5 md:text-8xl text-4xl text-white font-bold text-white text-center my-5'>DHISHNA</h1>
+        <h1 className='my-5 md:text-8xl text-4xl text-white font-bold text-white text-center my-5'>CAMPUS AMBASSADOR</h1>
         <p className='text-center text-white font-md lg:text-3xl md:text-2xl text-md my-5'>Be the emissary of Dhishna 2023</p>
         <div className='flex md:flex-row flex-col justify-center my-16'>
-            <div className='cursor-pointer group mr-10 md:px-16 px-5 py-4 bg-white hover:-translate-x-2 hover:-translate-y-2 transform transition-transform duration-200 ease-in-out mb-10' >
+            <div className='ml-10 cursor-pointer group mr-10 md:px-16 px-5 py-4 bg-white hover:-translate-x-2 hover:-translate-y-2 transform transition-transform duration-200 ease-in-out mb-10' >
 
                 {/* <div class="absolute top-0 left-0 w-8 h-8 bg-purple-950 transform rotate-45 -translate-x-7 -translate-y-5"></div>
 
@@ -31,7 +32,7 @@ const Hero = () => {
 
                 <p className='text-black text-center font-bold text-md md:text-xl'>SIGN UP</p>
             </div>
-            <div className='cursor-pointer group mr-10 md:px-16 px-5 py-4 bg-black bg-opacity-40 hover:-translate-x-2 hover:-translate-y-2 transform transition-transform duration-200 ease-in-out mb-10'>
+            <div className='ml-10 cursor-pointer group mr-10 md:px-16 px-5 py-4 bg-black bg-opacity-40 hover:-translate-x-2 hover:-translate-y-2 transform transition-transform duration-200 ease-in-out mb-10'>
                 {/* <div class="absolute top-0 left-0 w-8 h-8 bg-purple-950 transform rotate-45 -translate-x-5 -translate-y-5"></div>
 
                 <div class="absolute bottom-0 right-0 w-8 h-8 bg-purple-950 transform rotate-45 translate-x-5 translate-y-5"></div> */}
@@ -41,15 +42,15 @@ const Hero = () => {
         </div>
         <div className='flex justify-center my-20'>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-x-60 gap-y-10'>
-                <div className='px-10 py-12 rounded-[50%] bg-white'>
+                <div className='md:px-10 px-7 md:py-12 py-9 rounded-[50%] bg-white'>
                     <h3 className='text-black font-semibold text-5xl text-center'>{animateCountUp && <CountUp end={30} duration={3} />}+</h3>
                     <p className='text-black text-md my-2 text-center font-semibold'>WORKSHOPS</p>
                 </div>
-                <div className='px-10 py-10 rounded-[50%] bg-white'>
+                <div className='md:px-10 px-7 md:py-10 py-9 rounded-[50%] bg-white'>
                     <h3 className='text-black font-semibold text-5xl text-center'>{animateCountUp && <CountUp end={12} duration={4} />}+</h3>
                     <p className='text-black text-md my-2 text-center font-semibold'>LECTURES</p>
                 </div>
-                <div className='px-10 py-10 rounded-[50%] bg-white'>
+                <div className='md:px-10 px-7 md:py-10 py-9 rounded-[50%] bg-white'>
                     <h3 className='text-black font-semibold text-5xl text-center'>{animateCountUp && <CountUp end={44} duration={5} />}+</h3>
                     <p className='text-black text-md my-2 text-center font-semibold'>SPONSORS</p>
                 </div>
