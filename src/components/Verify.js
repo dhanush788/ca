1c88b8d3547fd8be85d5e2e84066fb3662d76674
Naveen Joy <naveenjoy18@gmail.com>
2023-09-20 22:52:55 +0530

    register - verify msg on sign up

---
+++ src/components/Verify.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Verify = () => {
+  return (
+    <div className="md:px-2 px-10">
+      <h1 className="text-white md:text-2xl sm:text-xl text-md text-center font-sans font-bold ">
+        You have successfully registered. Please wait for verification
+      </h1>
+    </div>
+  );
+};
+
+export default Verify;
