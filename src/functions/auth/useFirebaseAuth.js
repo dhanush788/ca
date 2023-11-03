c5b4af77e1b9798a8835c3ea681965e7b60576c4
Vaishnav <vaishnavvaichu123@gmail.com>
2023-11-03 19:14:24 +0530

    backend

--- src/functions/auth/useFirebaseAuth.js
+++ src/functions/auth/useFirebaseAuth.js
@@ -1,25 +1,38 @@
 import { useState, useEffect } from 'react'
 import { auth } from './firebase';
 
-const formatAuthUser = (user) => ({
+const formatAuthUser = (user, referral) => ({
     uid: user.uid,
     email: user.email,
-    token: user.accessToken
+    token: user.accessToken,
+    referral: referral
 });
-
+const updateReferral = (token, updater)=>{
+    fetch(process.env.REACT_APP_API_URL + "api/ca/getref", {
+            headers: {
+                Authorization: token
+            },
+            method: "POST",
+            // mode: 'no-cors',
+        }).then(res => res.json()).then((data => {
+            updater((old)=>({...old, referral: data.referral}))
+        }))
+}
 export default function useFirebaseAuth() {
     const [user, setAuthUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
-  
+
     const authStateChanged = async (authState) => {
         if (!authState) {
             setAuthUser(null)
             setLoading(false)
             return;
-        }   
-
-        var formattedUser = formatAuthUser(authState);
+        }
+        let referral = null;
+        updateReferral(authState.accessToken, setAuthUser)
+        
+        var formattedUser = formatAuthUser(authState, referral);
         setAuthUser(formattedUser);
         setLoading(false);
     };
